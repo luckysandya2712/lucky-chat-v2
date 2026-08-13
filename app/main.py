b558db0e0dc117e5e0e5e6fa5aa4ba971a63354b
luckysandya2712 <luckysandya2712@gmail.com>
2026-08-13 11:55:28 +0530

    Fix message timezone

--- app/main.py
+++ app/main.py
@@ -22,6 +22,7 @@ import asyncio
 import traceback
 from pathlib import Path
 from .notification import add_subscription
+from zoneinfo import ZoneInfo
 
 Base.metadata.create_all(bind=engine)
 
@@ -473,7 +474,7 @@ async def websocket_endpoint(websocket: WebSocket):
                     sender=username,
                     receiver=friend,
                     text=text,
-                    timestamp=datetime.now().strftime("%I:%M %p"),
+                    timestamp=datetime.now(ZoneInfo("Asia/Kolkata")).strftime("%I:%M %p")
                     unread=1,
                     seen_in_chat=0,
                     reply_to=data.get("reply_to"),
@@ -563,7 +564,7 @@ async def websocket_endpoint(websocket: WebSocket):
                         sender=username,
                         receiver=friend,
                         text=text,
-                        timestamp=datetime.now().strftime("%I:%M %p"),
+                        timestamp=datetime.now(ZoneInfo("Asia/Kolkata")).strftime("%I:%M %p")
                         unread=1,
                         seen_in_chat=0,
                         reply_to=data.get("reply_to"),
