3a6685c1271d55a3a7eb24783f1cef89ba2b8d88
luckysandya2712 <luckysandya2712@gmail.com>
2026-08-13 14:02:23 +0530

    Print database url

--- app/database.py
+++ app/database.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./luckychat.db")
 
+print("DATABASE_URL =", DATABASE_URL)
+
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
